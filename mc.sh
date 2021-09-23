aa6795b54b5bfa28364a7c03a0615ac6f2f653bc
Linwel Alexanderson <90960666+leanwell@users.noreply.github.com>
2021-09-23 15:02:30 +0800

    Add files via upload

---
+++ mc.sh
@@ -0,0 +1,62 @@
+#################################################
+# NOTE: MODIFY THE CODES BELOW BEFORE USING!    #
+# >>    Read READ.MD first before using         #
+# --------------------------------------------- #
+# Leanwell 2021                                 #
+# Basic bash script to execute TLauncher        #
+# github: leanwell | leanwell.github.io         #        
+# All Rights Reserved 2021                      #
+# -------------------------------------------- ##
+#################################################
+
+#! /bin/sh
+cd Downloads
+sudo java -jar TLauncher-2.82.jar # Edit the * in "TLauncher-*.jar" with the version of your own
+# enc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Leanwell 2019 | leanwell.github.io
+# ALL RIGHTS RESERVED 2021
