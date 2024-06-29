e424317dfba6b11f659cdf7540cda6dc96d6a794
Linwel C <90960666+leanwell@users.noreply.github.com>
2024-06-29 20:30:20 +0800

    Update mc.sh

--- mc.sh
+++ mc.sh
@@ -11,7 +11,7 @@
 
 #! /bin/sh
 cd Downloads
-sudo java -jar TLauncher-2.86.jar # Edit the * in "TLauncher-*.jar" with the version of your own
+java -jar TLauncher-2.86.jar # Edit the * in "TLauncher-*.jar" with the version of your own
 # enc
 
 
